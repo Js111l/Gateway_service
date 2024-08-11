040f7805712753f80f98b3bb00dd3bca6a70838c
Jakub <jakubswiercz5@gmail.com>
2024-08-11 17:37:54 +0200

    initial commit

---
+++ src/main/java/com/ecom/gateway/config/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.ecom.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RefreshScope
+@Component
+public class AuthenticationFilter implements GatewayFilter {
+
+    private final RouterValidator routerValidator;
+    private final TokenUtil jwtUtil;
+
+    @Autowired
+    public AuthenticationFilter(RouterValidator routerValidator, TokenUtil jwtUtil) {
+        this.routerValidator = routerValidator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if (routerValidator.isSecured.test(request)) {
+            if (this.isAuthMissing(request)) {
+                return this.onError(exchange, HttpStatus.UNAUTHORIZED);
+            }
+            final String token = this.getAuthHeader(request);
+            try {
+                jwtUtil.verifyToken(token);
+            }catch (Exception ex){
+                return this.onError(exchange, HttpStatus.FORBIDDEN);
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    private String getAuthHeader(ServerHttpRequest request) {
+        return request.getHeaders().getOrEmpty("Authorization").get(0);
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+
+}
